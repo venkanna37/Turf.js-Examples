9906c1a9d97334d4fad6d98d9f33a264362f1671
venkanna babu guthula <g.venkanna37@gmail.com>
2018-03-18 09:52:43 +0530

    second commit

--- along.js
+++
@@ -1,6 +0,0 @@
-var turf = require('turf');
-var fs = require('fs');
-var line = fs.readFileSync('./data/line.geojson');
-line = JSON.parse(line);
-//var turfLine = turf.lineString(line.features[2].coordinates);
-var alongOfLine = turf.along(line,200);
--- hexaGrid.js
+++
@@ -1,7 +0,0 @@
-var turf = require('turf');
-var fs = require ('fs');
-var bbox = [-71.6497,41.434,-70.4443,42.8886];
-var cellSide = 10;
-//var options = {units: 'kelometers'};
-var hexagrid = turf.hexGrid(bbox, cellSide );
-fs.writeFileSync('./boston_hexaGrid.geojson', JSON.stringify(hexagrid));
